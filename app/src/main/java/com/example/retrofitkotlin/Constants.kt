850a71443a2db0bd7f2e801d1bfe93ee3e3437db
SamuelRmos <samuelrmos.sjr@gmail.com>
2020-02-25 22:29:39 -0300

    Build service API

---
+++ app/src/main/java/com/example/retrofitkotlin/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.retrofitkotlin
+
+object Constants {
+    var tmdbApiKey = BuildConfig.TMDB_API_KEY
+}
---
+++ app/src/main/java/com/example/retrofitkotlin/service/RetrofitFactory.kt
@@ -0,0 +1,50 @@
+package com.example.retrofitkotlin.service
+
+import com.example.retrofitkotlin.BuildConfig
+import com.example.retrofitkotlin.Constants
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object RetrofitFactory {
+
+    private val authInterceptor = Interceptor { chain ->
+        val newUrl = chain.request().url()
+            .newBuilder()
+            .addQueryParameter("api_key", Constants.tmdbApiKey)
+            .build()
+        val newRequest = chain.request()
+            .newBuilder()
+            .url(newUrl)
+            .build()
+        chain.proceed(newRequest)
+    }
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client =
+        if (BuildConfig.DEBUG) {
+            OkHttpClient().newBuilder()
+                .addInterceptor(authInterceptor)
+                .addInterceptor(loggingInterceptor)
+                .build()
+        } else {
+            OkHttpClient().newBuilder()
+                .addInterceptor(loggingInterceptor)
+                .addInterceptor(authInterceptor)
+                .build()
+        }
+
+    fun retrofit(baseUrl : String) : Retrofit = Retrofit.Builder()
+        .client(client)
+        .baseUrl(baseUrl)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .addCallAdapterFactory(CoroutineCallAdapterFactory())
+        .build()
+
+}
