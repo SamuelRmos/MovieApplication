6dd3f0072806606c3009eafc7b9fe3d52eb98848
SamuelRmos <samuelrmos.sjr@gmail.com>
2020-04-02 23:13:18 -0300

    Adding view binding on activity

--- app/src/main/java/com/example/retrofitkotlin/ui/MovieActivity.kt
+++ app/src/main/java/com/example/retrofitkotlin/ui/MovieActivity.kt
@@ -2,16 +2,16 @@ package com.example.retrofitkotlin.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import com.example.retrofitkotlin.R
-import com.example.retrofitkotlin.viewmodel.MovieViewModel
+import com.example.retrofitkotlin.databinding.ActivityMovieBinding
 
 class MovieActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMovieBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie)
-
+        binding = ActivityMovieBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
