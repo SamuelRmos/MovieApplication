0eb0f9a6959d514bc26cbf82538b5aa4a2456403
Samuel Ramos <samuelramos.sjr@gmail.com>
2020-10-25 12:33:43 -0300

    Create converters file for CategoryEnum.kt and List class

---
+++ app/src/main/java/com/example/retrofitkotlin/converters/ListConverters.kt
@@ -0,0 +1,17 @@
+//package com.example.retrofitkotlin.converters
+//
+//import androidx.room.TypeConverter
+//import com.example.retrofitkotlin.model.TmdMovie
+//
+//class ListConverters {
+//    @TypeConverter
+//    fun jsonToIntList(json: String): MutableList<TmdMovie> {
+////        return Gson().fromJson(json, Array<TmdMovie>::class.java).toMutableList()
+//    }
+//
+//    @TypeConverter
+//    fun intListToJson(objList: MutableList<TmdMovie>): String {
+////        return Gson().toJson(objList)
+//    }
+//
+//}
